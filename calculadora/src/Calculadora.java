a915965d05c7364a0e38acba91496f159bb3caf1
VenioFerreira1973 <venioferreira@gmail.com>
2023-04-19 18:07:17 -0300

    Atualizando arquivos referentes a java docs

---
+++ calculadora/src/Calculadora.java
@@ -0,0 +1,24 @@
+/**
+ * <h1>Calculadora</h1>
+ * A calculadora realiza operações matematicas entre números inteiros
+ * <p>
+ * <b>Note: </b> Leia atentamente a documentação desta classe
+ * para desfrutar dos recursos oferecidos pelo autor
+ * 
+ * @author Vênio Ferreira
+ * @version 1.0
+ * @since 19/04/2023
+ */
+public class Calculadora {
+
+    /**
+     * Este método é utilizado para somar dois números inteiros
+     * 
+     * @param numeroUm   este é o primeiro parametro
+     * @param numeroDois este é o segunto parametro
+     * @return int o resultado do método é a soma dos dois numeros
+     */
+    public int somar(int numeroUm, int numeroDois) {
+        return numeroUm + numeroDois;
+    }
+}
