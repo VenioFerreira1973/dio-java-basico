0fd7d1b5c80594c26fe3dfbf723fcd8e5d19963a
VenioFerreira1973 <venioferreira@gmail.com>
2023-04-19 18:06:24 -0300

    Atualizacao de documentos referentes a Java docs

---
+++ calculadora/src/Comentarios.java
@@ -0,0 +1,19 @@
+public class Comentarios {
+
+    public static void main(String[] args) {
+        //Ola eu sou um comentario de unica linha
+
+        /*
+         * Olá eu sou um comentario
+         * de multiplas linhas
+         */
+
+         /**
+          * Estes dois asteriscos acima é para identificar que você
+          * pretende elaborar um comentário a nivel de documentação
+          */
+          public void metodo(){
+            
+          }
+    }
+}
